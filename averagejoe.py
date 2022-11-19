c41c7f30701216adcce5d26e1bc9cb157ff31bf9
SC <schittegoppe@gmail.com>
2022-11-19 14:28:15 -0500

    11-19-22:2:38

--- averagejoe.py
+++ averagejoe.py
@@ -4,14 +4,16 @@
  #   with open(playercombinedata.csv) as nflcombine:
  #       nflcombine_data = csv.reader(playercombinedata.csv, delimiter=',')
  #       for row in nflcombine_data:
+
+import csv
+
+
 u_mile = int(input("What is your current fastest mile, rounded up, in minutes?"))
 #convert mile into 40 yard dash time
 # need to divide miles into yards
 u_fr = str(u_mile * .04)
 print ('Your yards per minute is: ' + u_fr)
 
-
-import csv
 
 
 # 11-18-22: Need to learn how to return row about player from input 
