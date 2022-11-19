68197df0bf66937462f49b98a3a7052e734011f5
SC <schittegoppe@gmail.com>
2022-11-18 21:16:25 -0500

    new update 11-18-09-16-PM

--- averagejoe.py
+++ averagejoe.py
@@ -1,11 +1,8 @@
-from datetime import time
-
+#from datetime import time
 # asking user for fastest mile and storing it as a variable
 u_mile = int(input("What is your current fastest mile, rounded up, in minutes?"))
-
 #convert mile into 40 yard dash time
 # need to divide miles into yards
 u_fr = str(u_mile * )
-print ('Your yards per minute is: ' + u_fr)
-
+#print ('Your yards per minute is: ' + u_fr)
 
