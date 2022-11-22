65b03bb6fae299884880fb729b9558f7ae78b180
SC <schittegoppe@gmail.com>
2022-11-21 21:49:44 -0500

    new_csv
    Created new .csv file from combine data frame

--- combinecsvfiles.py
+++ combinecsvfiles.py
@@ -8,11 +8,18 @@ nfl = pd.read_csv("playercombinedata.csv")
 nfl2022 = pd.read_csv("playercombinedata2022.csv") 
 nfl2022.insert(0,'Year',2022,allow_duplicates= False)
 
-nfl2022.rename(columns= {'Player':'Name', 'School':'College', 'College': 'Secondary Type', 'Ht':'Height'})
+nfl2022.rename(columns= {'WT':'Weight(lbs)', '40yd': '40 Yard', 'Vertical':'Vert Leap(in)',
+                            'Broad Jump':'Broad Jump(in)', 'Bench':'Bench Pres',
+                            'Player': 'Name', 'College': 'Secondary Type', 'Ht': 'Height (in)',
+                            'School': 'College', 'Pos': 'POS', 
+                             }, inplace = True)
 
-print (nfl2022.columns)
-print (nfl.columns)
+nfl_df = pd.concat([nfl,nfl2022],axis = 1, join = 'outer')
 
+# print (nfl_df)
+#print(nfl_df.columns)
+
+nfl_df.to_csv('nfl_combine_csv')
 
 
 
